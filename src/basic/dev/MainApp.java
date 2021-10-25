b1882d2987a4ea43e310d8286867a179cee76464
Vanluom <lluom14@gmail.com>
2021-10-25 14:34:15 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,51 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/**
+		 * thông tin một sinh viên
+		 */
+		String ten = "Le van Luom ";
+		String msv = "21602166";
+		String cmnd = "191929545";
+		String tentruong = "HueIC";
+		float dtb = 8.1f;
+		
+		System.out.println("Ten sinh vien: " + ten);
+		System.out.println("Ma sinh vien: " + msv);
+		System.out.println("Chung minh nhan dan:  " + cmnd);
+		System.out.println("Ten Truong:  " + tentruong);
+		System.out.println("diem trung binh: " + dtb);
+		/**
+		 * thong tin cong nhan
+		 */
+		String hoten = "Le Van luom ";
+		String diachi = " 88 nguyen phuc thai ";
+		String sdt = "0962677637";
+		float hsl = 6.0f;
+		
+		System.out.println("Ten cong nhan : " + hoten);
+		System.out.println("diachi: " + diachi);
+		System.out.println("sdt: " + sdt);
+		System.out.println("He so luong : " + hsl);
+		
+		/**
+		 * thong tin mot chiec xe
+		 */
+		
+		String tenxe = "xe";
+		String hang = "contect";
+		float gia = 7.8f;
+		String mauson = "Do";
+		
+		System.out.println("Ten xe : " + tenxe);
+		System.out.println("hang : " + hang);
+		System.out.println("Mau son  : " + mauson);
+		System.out.println("gia:" + gia);
+		
+		
+	}
+
+}
